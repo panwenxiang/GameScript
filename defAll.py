a2f226d2ca6e495d0560d38c87b87577c2797749
panwenxiangHome <1845037848@qq.com>
2022-11-22 23:24:22 +0800

    重新截图，设置夜神模拟器大小

--- defAll.py
+++ defAll.py
@@ -1,12 +1,12 @@
 import cv2
-import win32gui
+import win32gui, win32con
 from PyQt5.QtWidgets import QApplication
 import sys
 import qimage2ndarray
 
 
 # 查找窗口方法
-def get_window(name):
+def get_handle(name):
     # 通过名字或类名获取窗口句柄
     handle = win32gui.FindWindow(0, name) | win32gui.FindWindow(name, None)
     if handle == 0:
@@ -16,12 +16,20 @@ def get_window(name):
         return handle
 
 
-# handle-句柄
-# 获取截图并转灰
-def get_screenshot(handle):
+def set_window(handle, uFlags):
+    win32gui.SetWindowPos(handle, win32con.HWND_TOP, 0, 0, 596, 1020, uFlags)
+    # 夜神 0, 0, 596, 1020
+
+
+# handle-句柄; 获取截图并转灰
+def get_screenshot(handle, is_gray=None):
     if handle:
-        # 窗口放到最上层
-        # win32gui.SetWindowPos(handle, win32con.HWND_TOPMOST, 0, 0, 1200, 800, win32con.SWP_SHOWWINDOW)
+        # 打开窗口，得保持窗口是打开状态
+        set_window(handle, win32con.SWP_NOMOVE)
+
+        # 获取坐标
+        (x1, y1, x2, y2) = win32gui.GetWindowRect(handle)
+        print(x1, y1, x2, y2)
 
         # Qt5截图
         app = QApplication(sys.argv)
@@ -32,7 +40,8 @@ def get_screenshot(handle):
         imgz = qimage2ndarray.rgb_view(img)
 
         imgz = cv2.cvtColor(imgz, cv2.COLOR_BGR2RGB)  # BGR转RGB
-        # imgz = cv2.cvtColor(imgz, cv2.COLOR_BGR2GRAY)  # BGR转灰度
+        if is_gray:
+            imgz = cv2.cvtColor(imgz, cv2.COLOR_BGR2GRAY)  # BGR转灰度
         return imgz
 
     # 窗口放到最上层
--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 import cv2
+import win32con
 import defAll
 
-handle = defAll.get_window('夜神模拟器')
-print('获取的句柄:', handle, hex(eval(str(handle))))
+handle = defAll.get_handle('夜神模拟器-小号')
+# handle = defAll.get_handle('文件传输助手')
 
 if handle:
-    imgz = defAll.get_screenshot(handle)
+    print('获取的句柄:', handle, hex(eval(str(handle))))
+    # defAll.set_window(handle, win32con.SWP_SHOWWINDOW)
+    imgz = defAll.get_screenshot(handle, 'gray')
+
     cv2.imshow('2222', imgz)
     cv2.waitKey()
 
